286b0c6ac017c1b066a77624eaac105fb4396db2
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2024-06-28 15:16:35 +0530

    for each loop sample

---
+++ class01.java
@@ -0,0 +1,15 @@
+/**
+ * class01
+ */
+public class class01 {
+
+    public static void main(String[] args){
+
+        int[] age = {12,4,5};
+
+        System.out.println("Use of for each loop: ");
+        for(int i: age){
+            System.out.println(i);
+        }
+    }
+}
