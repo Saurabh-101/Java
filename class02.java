ecb745ff6ff5f735334c97f4a30b352f09ec5cf7
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2024-07-01 23:11:29 +0530

    adding new file

---
+++ class02.java
@@ -0,0 +1,8 @@
+public class class02 {
+
+    public static void main(String[] args){
+        System.out.println("Can.t resolve an error in redux-prsist libraby and irritated as hell")
+    }
+
+
+}
